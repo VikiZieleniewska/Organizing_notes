2c6b651b4a004b0f7f81a08405a04ec6f65ab01e
Viki Zieleniewska <viktoriya.zieleniewska@gmail.com>
2023-06-08 19:07:32 +0200

    added edit mode to NoteCard

--- src/components/NoteCard/NoteCard.jsx
+++ src/components/NoteCard/NoteCard.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import { Alert, Button, TextField, Typography } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
@@ -10,40 +11,135 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import CloseIcon from "@mui/icons-material/Close";
+import SaveIcon from "@mui/icons-material/Save";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
-function NoteCard({ note }) {
-  return (
-    <Card>
-      <CardHeader
-        action={
-          <>
-            <IconButton aria-label="edit">
-              <EditIcon />
-            </IconButton>
-            <IconButton aria-label="delete">
-              <DeleteIcon />
-            </IconButton>
-          </>
-        }
-        title={note.title}
-      />
-      <CardContent>
-        <List>
-          {note.tasks.map((task) => (
-            <ListItem disablePadding key={task.id}>
-              <ListItemButton>
-                <ListItemIcon>
-                  <CheckCircleOutlineIcon />
-                </ListItemIcon>
-                <ListItemText primary={task.description} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </CardContent>
-    </Card>
-  );
-}
+export default class NoteCard extends Component {
+  state = {
+    note: JSON.parse(JSON.stringify(this.props.note)),
+    isEditMode: false,
+  };
 
-export default NoteCard;
+  // componentDidUpdate() {}
+
+  toggleEditMode = () => this.setState({ isEditMode: !this.state.isEditMode });
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(`submitted form`);
+  };
+
+  handleCancel = () => {
+    this.setState({
+      note: {
+        ...this.props.note,
+      },
+      isEditMode: false,
+    });
+  };
+
+  handleDelete = async (event) => {
+    console.log(`note deleted`);
+  };
+
+  render() {
+    if (this.state.isEditMode) {
+      return (
+        <Card>
+          <form onSubmit={this.handleSubmit}>
+            <CardHeader
+              action={
+                <>
+                  <IconButton type="submit" aria-label="save">
+                    <SaveIcon />
+                  </IconButton>
+                  <IconButton aria-label="cancel" onClick={this.handleCancel}>
+                    <CloseIcon />
+                  </IconButton>
+                </>
+              }
+              title={
+                <TextField
+                  id="title"
+                  variant="standard"
+                  fullWidth
+                  value={this.state.note.title}
+                  onChange={(event) => {
+                    this.setState({
+                      note: {
+                        ...this.state.note,
+                        title: event.target.value,
+                      },
+                    });
+                  }}
+                  // sx={{ marginBottom: "20px" }}
+                />
+              }
+            />
+            <CardContent>
+              <List>
+                {this.state.note.tasks.map((task) => (
+                  <ListItem disablePadding key={task.id}>
+                    <TextField
+                      id={`task-${task.id}`}
+                      variant="standard"
+                      fullWidth
+                      value={task.description}
+                      onChange={(event) => {
+                        const updatedTasks = [...this.state.note.tasks];
+                        updatedTasks.find(
+                          (el) => el.id === task.id
+                        ).description = event.target.value;
+
+                        this.setState({
+                          note: {
+                            ...this.state.note,
+                            tasks: updatedTasks,
+                          },
+                        });
+                      }}
+                      // sx={{ marginBottom: "20px" }}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </CardContent>
+          </form>
+        </Card>
+      );
+    }
+
+    return (
+      <Card>
+        <CardHeader
+          action={
+            <>
+              <IconButton aria-label="edit" onClick={this.toggleEditMode}>
+                <EditIcon />
+              </IconButton>
+              <IconButton aria-label="delete" onClick={this.handleDelete}>
+                <DeleteIcon />
+              </IconButton>
+            </>
+          }
+          title={this.state.note.title}
+        />
+        <CardContent>
+          <List>
+            {this.state.note.tasks.map((task) => (
+              <ListItem disablePadding key={task.id}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <CheckCircleOutlineIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={task.description} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </CardContent>
+      </Card>
+    );
+  }
+}
